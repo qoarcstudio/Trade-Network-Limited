1c1e23490483a2baee0bc80e8b344cd2d29c892f
Abrar Al Sayem <abraralsayem97@gmail.com>
2026-03-09 08:05:04 +0600

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/Trade-Network-Limited' : '',
+  assetPrefix: isProd ? '/Trade-Network-Limited/' : '',
 };
 
 export default nextConfig;
